f511eb6dca07faffca7911a26aa257e5d5139009
duchaba <duc.haba@gmail.com>
2022-09-30 05:24:26 +0000

    chapter 2 complete

--- pluto/pluto_chapter_2.py
+++ pluto/pluto_chapter_2.py
@@ -82,3 +82,172 @@ def say_sys_info(self):
     pass
   self._ph()
   return
+
+# easy method to download kaggle data files
+!pip install opendatasets --upgrade
+import opendatasets
+print("\nrequired version 0.1.22 or higher: ", opendatasets.__version__)
+
+@add_method(PacktDataAug)
+def remember_kaggle_access_keys(self,username,key):
+  self.kaggle_username = username
+  self.kaggle_key = key
+  return
+
+@add_method(PacktDataAug)
+def _write_kaggle_credit(self):
+  creds = '{"username":"'+self.kaggle_username+'","key":"'+self.kaggle_key+'"}'
+  kdirs = ["~/.kaggle/kaggle.json", "./kaggle.json"]
+  #
+  for k in kdirs:
+    cred_path = pathlib.Path(k).expanduser()
+    cred_path.parent.mkdir(exist_ok=True)
+    cred_path.write_text(creds)
+    cred_path.chmod(0o600)
+  import kaggle
+  #
+  return
+#
+@add_method(PacktDataAug)
+def fetch_kaggle_comp_data(self,cname):
+  self._write_kaggle_credit()
+  path = pathlib.Path(cname)
+  kaggle.api.competition_download_cli(str(path))
+  zipfile.ZipFile(f'{path}.zip').extractall(path)
+  return
+#
+#
+@add_method(PacktDataAug)
+def fetch_kaggle_dataset(self,url,dest="kaggle"):
+  self._write_kaggle_credit()
+  opendatasets.download(url,data_dir=dest)
+  return
+
+import kaggle
+import zipfile
+import os
+
+@add_method(PacktDataAug)
+def fetch_df(self, csv):
+  df = pandas.read_csv(csv)
+  return df
+
+@add_method(PacktDataAug)
+def build_sf_fname(self, df):
+  root = 'state-farm-distracted-driver-detection/imgs/train/'
+  df["fname"] = root + df.classname + '/' + df.img
+  return
+
+# set internal counter for image to be zero, e.g. pluto0.jpg, pluto1.jpg, etc.
+pluto.fname_id = 0
+
+@add_method(PacktDataAug)
+def _drop_image(self,canvas, fname=None,format=".jpg",dname="Data-Augmentation-with-Python/pluto_img"):
+  if (fname is None):
+    self.fname_id += 1
+    if not os.path.exists(dname):
+      os.makedirs(dname)
+    fn = dname + "/pluto" + str(self.fname_id) + format
+  else:
+    fn = fname
+  canvas.savefig(fn, cmap="Greys", bbox_inches="tight", pad_inches=0.25)
+  return
+#
+@add_method(PacktDataAug)
+def draw_batch(self,df_filenames, disp_max=10,is_shuffle=False, figsize=(16,8)):
+  disp_col = 5
+  disp_row = int(numpy.round((disp_max/disp_col)+0.4, 0))
+  _fns = list(df_filenames)
+  if (is_shuffle):
+    numpy.random.shuffle(_fns)
+  k = 0
+  clean_fns = []
+  if (len(_fns) >= disp_max):
+    canvas, pic = matplotlib.pyplot.subplots(disp_row,disp_col, figsize=figsize)
+    for i in range(disp_row):
+      for j in range(disp_col):
+        try:
+          im = PIL.Image.open(_fns[k])
+          pic[i][j].imshow(im)
+          pic[i][j].set_title(pathlib.Path(_fns[k]).name)
+          clean_fns.append(_fns[k])
+        except:
+          pic[i][j].set_title(pathlib.Path(_fns[k]).name)
+        k += 1
+    canvas.tight_layout()
+    self._drop_image(canvas)
+    canvas.show()
+  else:
+    print("**Warning: the length should be more then ", disp_max, ". The given length: ", len(_fns))
+  return clean_fns
+
+import os
+#
+@add_method(PacktDataAug)
+def build_shoe_fname(self, start_path):
+  df = pandas.DataFrame()
+  for root, dirs, files in os.walk(start_path, topdown=False):
+   for name in files:
+      f = os.path.join(root, name)
+      p = pathlib.Path(f).parent.name 
+      d = pandas.DataFrame({'fname': [f], 'label': [p]})
+      df = df.append(d, ignore_index=True)
+  #
+  # clean it up
+  df = df.reset_index(drop=True)
+  return df
+
+@add_method(PacktDataAug)
+def print_batch_text(self,df_orig, disp_max=10, cols=["title", "description"]): 
+  df = df_orig[cols] 
+  with pandas.option_context("display.max_colwidth", None):
+    display(df.sample(disp_max))
+  return
+
+@add_method(PacktDataAug)
+def count_word(self, df, col_dest="description"):
+  df['wordc'] = df[col_dest].apply(lambda x: len(x.split()))
+  return
+
+@add_method(PacktDataAug)
+def draw_word_count(self,df, wc='wordc'):
+  canvas, pic = matplotlib.pyplot.subplots(1,2, figsize=(16,5))
+  df.boxplot(ax=pic[0],column=[wc],vert=False,color="black")
+  df[wc].hist(ax=pic[1], color="cornflowerblue", alpha=0.9)
+  #
+  title=["Description BoxPlot", "Description Histogram"]
+  yaxis=["Description", "Stack"]
+  x1 = "Word Count: Mean: " + str(round(df[wc].mean(), 2)) + ", Min: " + str(df[wc].min()) + ", Max: " + str(df[wc].max())
+  xaxis=[x1, "Word Count"]
+  #
+  pic[0].set_title(title[0], fontweight ="bold")
+  pic[1].set_title(title[1], fontweight ="bold")
+  pic[0].set_ylabel(yaxis[0])
+  pic[1].set_ylabel(yaxis[1])
+  pic[0].set_xlabel(xaxis[0])
+  pic[1].set_xlabel(xaxis[1])
+  #
+  canvas.tight_layout()
+  self._drop_image(canvas)
+  # 
+  canvas.show()
+  return
+
+!pip install pyspellchecker 
+import spellchecker
+import re
+#spell_check = spellchecker.SpellChecker()
+pluto._ph()
+pluto._pp("Required version 0.7+", spellchecker.__version__)
+
+@add_method(PacktDataAug)
+def _strip_punc(self,s):
+  p = re.sub(r'[^\w\s]','',s)
+  return(p)
+#
+@add_method(PacktDataAug)
+def check_spelling(self,df, col_dest='description'):
+  spell = spellchecker.SpellChecker()
+  df["misspelled"] = df[col_dest].apply(lambda x: spell.unknown(self._strip_punc(x).split()))
+  df["misspelled_count"] = df["misspelled"].apply(lambda x: len(x))
+  return
