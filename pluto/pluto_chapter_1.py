6dfc655e4b53d8d0c516249603ef6c697aec0cd1
duc <duc.....com>
2023-08-04 21:32:02 +0000

    update with latest ai doc

--- pluto/pluto_chapter_1.py
+++ pluto/pluto_chapter_1.py
@@ -1,4 +1,10 @@
 
+# AI function documentation
+#
+# prompt: write documentation for the following function: add_method()
+# Note: B-grade, cycle the above to one function at a time. Give multiple
+# functions will confused it.
+
 # create an object
 # First, importing the basic library
 import torch
@@ -10,11 +16,38 @@ import PIL
 import datetime
 import sys
 import psutil
-# create class/object 
+# create class/object
 class PacktDataAug(object):
+  """
+    The PacktDataAug class is the based class for the
+    "Data Augmentation with Python" book.
+  """
   #
   # initialize the object
   def __init__(self, name="Pluto", is_verbose=True,*args, **kwargs):
+    """
+
+    This is the constructor function.
+
+    Args:
+
+     name (str): It requires a name for the object. The default is 'Pluto'
+     verbose (bool):  The default value of `verbose` is True. This function prints out the
+        name of the object if `is_verbose == True`. This is used to debug
+        code. When you are ready to deploy the model, then you should set
+        `is_verbose == False` in order to avoid printing out diagnostic
+        messages.
+
+      Additionally, this function takes any number of other
+      parameters. These parameters are stored in `**kwargs` and are
+      accessed via the function `get_kwargs()`. See the documentation
+      for `get_kwargs()` for more details.
+      Note that `__init__()` is
+      automatically called when you create a new object.
+
+    Returns:
+      None.
+    """
     super(PacktDataAug, self).__init__(*args, **kwargs)
     self.author = "Duc Haba"
     self.version = 1.0
@@ -30,34 +63,85 @@ class PacktDataAug(object):
   #
   # pretty print output name-value line
   def _pp(self, a, b):
+    """
+
+      pretty print output name-value line
+
+      Args:
+          a (str): Name of key
+          b (any): value of key
+
+      Returns:
+          None
+    """
     print("%28s : %s" % (str(a), str(b)))
     return
   #
   # pretty print the header or footer lines
   def _ph(self):
+    """
+      pretty print the header or footer lines
+
+      Args:
+          None
+
+      Returns:
+          None
+      """
     print("-" * 28, ":", "-" * 28)
     return
 # ---end of class
 #
 # Hack it! Add new decorator
-# add_method() is inspired Michael Garod's blog, 
+# add_method() is inspired Michael Garod's blog,
 # AND correction by: Филя Усков
 #
 import functools
 def add_method(x):
+  """
+
+    Decorator creates a new method to class
+    `x` with the same name and parameters as function `z`
+    Args:
+        x: class to add function
+        z: function to add to class `x`
+    Returns:
+        a decorator
+  """
   def dec(z):
-    @functools.wraps(z) 
-    def y(*args, **kwargs): 
+    @functools.wraps(z)
+    def y(*args, **kwargs):
       return z(*args, **kwargs)
     setattr(x, z.__name__, y)
-    return z 
+    return z
   return dec
 #
 
+# create pluto (or any name you choose)
 pluto = PacktDataAug("Pluto")
 
 @add_method(PacktDataAug)
 def say_sys_info(self):
+  """
+
+    Print out system information. Useful for
+    debugging purposes. Prints out information such as
+    the system time, platform, Python version, PyTorch
+    version, Pandas version, PIL version, and
+    Matplotlib version. Also prints the number of CPU
+    cores and the CPU speed.
+
+    Note that this function is added to the class `PacktDataAug` via
+    the decorator `@add_method()`. This means that you can
+    call this function as `p.say_system_info()`,
+    where `p` is an instance of `PacktDAtaAug`.
+
+    Args:
+      None
+
+    Returns:
+      None
+  """
   self._ph()
   now = datetime.datetime.now()
   self._pp("System time", now.strftime("%Y/%m/%d %H:%M"))
@@ -77,7 +161,7 @@ def say_sys_info(self):
     if (None != val):
       val = val._asdict()
       self._pp("CPU speed",  f'{val["current"]/1000:.2f} GHz')
-      self._pp("CPU max speed", f'{val["max"]/1000:.2f} GHz') 
+      self._pp("CPU max speed", f'{val["max"]/1000:.2f} GHz')
     else:
       self._pp("*CPU speed", "NOT available")
   except:
